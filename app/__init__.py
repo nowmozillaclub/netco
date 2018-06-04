07f386b71d2ef3c7ca38bb595d3d168620642ca4
InfernapeXavier <itsgood37@ymail.com>
2018-06-04 21:00:17 +0530

    Netcofication
    Changed all mentions of Webby to Netco

--- app/__init__.py
+++ app/__init__.py
@@ -33,7 +33,7 @@ if not app.debug:
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Webby! Failure',
+            toaddrs=app.config['ADMINS'], subject='Netco! Failure',
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
@@ -44,14 +44,14 @@ if not app.debug:
 
 if not os.path.exists('logs'):
     os.mkdir('logs')
-file_handler = RotatingFileHandler('logs/webby.log', maxBytes=10240, backupCount=10)
+file_handler = RotatingFileHandler('logs/netco.log', maxBytes=10240, backupCount=10)
 file_handler.setFormatter(logging.Formatter(
     '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
 file_handler.setLevel(logging.INFO)
 app.logger.addHandler(file_handler)
 
 app.logger.setLevel(logging.INFO)
-app.logger.info('Webby Startup')
+app.logger.info('Netco Startup')
 
 @babel.localeselector
 def get_locale():
